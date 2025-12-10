e9e051ab7cbc618fdcdd1d21d15732551d649787
lijinglin <nextuser@163.com>
2025-12-10 20:20:05 +0800

    touchable opacity

--- blank-demo/components/shared/net-error.jsx
+++ blank-demo/components/shared/net-error.jsx
@@ -1,10 +1,16 @@
 import {View,Text,Button,StyleSheet} from 'react-native';
 import { SimpleLineIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 export default function NetError(props){
    return  <View style={styles.container_center}>
       <SimpleLineIcons name={"drawer"} size={160} color={"#d31d1dff"} />
       <Text style={styles.textRed}>{props.title}</Text>
-    <Button title="Reload" onPress={()=>props.reload()}/>  
+
+      <TouchableOpacity style={styles.reload} onPress={()=>props.reload()}>
+        <Text style={styles.buttonText}>Reload</Text>
+      </TouchableOpacity>
+      
+    
     </View>
 }
 
@@ -19,5 +25,16 @@ const styles = StyleSheet.create({
     color: '#ddd',
     fontSize: 16,
     marginTop: 20
+  },
+    reload: {
+    marginTop: 10,
+    backgroundColor: '#1f99b0',
+    justifyContent: 'center',
+    height: 40,
+    borderColor:"black",
+    borderWidth: 2,
+    borderRadius: 4,
+    paddingLeft: 10,
+    paddingRight: 10,
   }
 });
