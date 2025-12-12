4a22d922c2bc7e0140bbe3e6023052832bb3bf7c
lijinglin <nextuser@163.com>
2025-12-12 16:15:06 +0800

    modal close button

--- tab-demo/app/_layout.js
+++ tab-demo/app/_layout.js
@@ -1,4 +1,7 @@
+import { MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons'
 import {Stack} from 'expo-router'
+import { useRouter } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 
 export default function AppLayout(){
     return (
@@ -9,11 +12,12 @@ export default function AppLayout(){
                 },
                 headerTintColor:"#1f99b0",
                 headerTitleStyle: {
-                fontWeight: '400',
-                color: '#2A2929',
-                fontSize: 16,
+                    fontWeight: '400',
+                    color: '#2A2929',
+                    fontSize: 16,
                 },
                 headerBackButtonDisplayMode:'minimal',
+                headerLeft:()=><CloseButton/>
             }}
         
         >   <Stack.Screen name="(tabs)" options={{
@@ -36,6 +40,24 @@ export default function AppLayout(){
             <Stack.Screen name = "notifications/index" options={{
                 title:"通知"
             }}/>
+            <Stack.Screen name = "teachers/[id]" options={{
+                title:"老师",
+                presentation:"fullScreenModal",
+            }}/>
         </Stack>
+    )
+}
+
+function CloseButton(){
+    const router = useRouter();
+    return (
+        <TouchableOpacity style={{
+            padding:10,
+            marginRight:10,
+        }} onPress={()=>{
+            router.dismiss();
+        }}>
+        <MaterialCommunityIcons name="close" size={20} color="#000"/>
+        </TouchableOpacity>
     )
 }
